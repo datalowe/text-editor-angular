9efb551f87d626671096504c15fdec369eabf35e
datalowe <datalowe@posteo.de>
2021-09-16 18:16:46 +0200

    fix automatically generated ng tests. (remove one irrelevant test, add HttpClientModule imports for 2)

--- src/app/components/editor-area/editor-area.component.spec.ts
+++ src/app/components/editor-area/editor-area.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { EditorAreaComponent } from './editor-area.component';
 
@@ -8,7 +9,12 @@ describe('EditorAreaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditorAreaComponent ]
+      imports: [
+        HttpClientModule
+      ],
+      declarations: [ 
+        EditorAreaComponent 
+      ]
     })
     .compileComponents();
   });
--- src/app/services/document.service.spec.ts
+++ src/app/services/document.service.spec.ts
@@ -1,9 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { DocumentService } from './document.service';
 
 describe('DocumentService', () => {
   let service: DocumentService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
