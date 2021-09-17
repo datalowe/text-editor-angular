c0459e2e860a9ff5f7a0852f4012b6723eed9815
datalowe <datalowe@posteo.de>
2021-09-17 11:11:51 +0200

    add editor toolbar component test involving test of rendered template parts.

--- src/app/components/editor-toolbar/editor-toolbar.component.spec.ts
+++ src/app/components/editor-toolbar/editor-toolbar.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ButtonComponent } from '../button/button.component';
 
 import { EditorToolbarComponent } from './editor-toolbar.component';
@@ -6,6 +8,7 @@ import { EditorToolbarComponent } from './editor-toolbar.component';
 describe('EditorToolbarComponent', () => {
   let component: EditorToolbarComponent;
   let fixture: ComponentFixture<EditorToolbarComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,9 +24,18 @@ describe('EditorToolbarComponent', () => {
     fixture = TestBed.createComponent(EditorToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    de = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have showDocList set to false after creation', () => {
+    expect(component.showDocList).toBeFalsy();
+  });
+
+  it('should have three buttons', () => {
+    expect(de.queryAll(By.css('app-button')).length).toEqual(3);
   });
 });
