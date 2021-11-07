9dd68e2c3b3df70a01f5135a0183e3f500ab568f
datalowe <datalowe@posteo.de>
2021-11-07 14:24:38 +0100

    update cypress registration test to make it not rely on backend running

--- cypress/integration/test-login-registration.ts
+++ cypress/integration/test-login-registration.ts
@@ -6,6 +6,10 @@ const registrationUser: PlainUser = {
   password: "disney"
 };
 const apiUrl = `${backendRootUrl}/user`;
+const fakeUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+  'eyJ1c2VybmFtZSI6Imxvd2UiLCJ1c2VySWQiOiI2MTdiYWE2ZWM1ZjFh' +
+  'MmQ0ZmZiMjQ3Y2QiLCJpYXQiOjE2MzYyOTEyOTcsImV4cCI6MTYzNjI5NDg5N30.' +
+  'pNP1C7tUNDynOB06aCNb7z2CJ1OQ-_7eMkBjvmaXZ_4';
 
 describe('Login form', () => {
     it('Initial page shows login form for non-logged in user', () => {
@@ -21,9 +25,19 @@ describe('Registration form', () => {
     });
 
     it('Correctly attempts to send POST request to backend upon valid form submission', () => {
+      // intercept request to backend when registering new user
       cy.intercept('POST', `${apiUrl}/register`, {
         statusCode: 201
       }).as('requestNewUser');
+
+      // intercept request to backend when attempting to log in user
+      cy.intercept('POST', `${apiUrl}/login`, {
+        statusCode: 201,
+        body: {
+          token: fakeUserToken
+        }
+      }).as('loginNewUser');
+
       cy.visit('/register');
 
       const usernameInput = cy.get('#username-input');
